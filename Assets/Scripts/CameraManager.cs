3bf309652b6899b2ca4bfecfb1b1778c44b4b026
arthur14038 <arthur14038@gmail.com>
2020-09-24 00:23:02 +0800

    新增專案

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject m_Player;
+
+    Vector3 mFollowDistance;
+
+    void Start()
+    {
+        mFollowDistance = new Vector3(0f, 3f, -7.2f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.forward = m_Player.transform.forward;
+        this.transform.position = m_Player.transform.position + m_Player.transform.TransformDirection(mFollowDistance);
+    }
+}
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : SingletonMonoBehavior<InputManager>
+{
+    public float Horizontal;
+    public float Vertical;
+
+    public Action<float, float> MoveDataUpdate;
+    public Action OnJumpDown;
+    public Action<float> OnVerticalChanged;
+    public Action<float> OnHorizontalChanged;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+
+        Vertical = Input.GetAxis("Vertical");
+
+        MoveDataUpdate?.Invoke(Horizontal, Vertical);
+
+        if (Input.GetButtonDown("Jump"))
+            OnJumpDown?.Invoke();
+    }
+
+    //public void OnVerticalChanged()
+    //{
+
+    //}    
+}
